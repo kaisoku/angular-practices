700fe4d31dd499210e2f5d231558c5f465c37100
Kaisoku <khuss063@gmail.com>
2023-04-05 17:24:51 +0200

    update package

--- src/app/alert/successAlert/success.component.ts
+++ src/app/alert/successAlert/success.component.ts
@@ -8,7 +8,7 @@ import { Component } from "@angular/core";
       p {
         color: green;
       }
-    `
-  ]
+    `,
+  ],
 })
 export class SuccessAlertComponent {}
--- src/app/alert/warningAlert/warning.component.ts
+++ src/app/alert/warningAlert/warning.component.ts
@@ -3,6 +3,6 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-warningalert",
   templateUrl: "./warning.component.html",
-  styleUrls: ["./warning.component.css"]
+  styleUrls: ["./warning.component.css"],
 })
 export class WarningAlertComponent {}
